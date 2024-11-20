08fe4f96944fc202e013b35575aa31c60a54dd20
Loreks19 <brawl.boy444@gmail.com>
2024-11-20 19:08:35 +0200

    Lab4 done

--- lab3/task1/src/App.js
+++ lab3/task1/src/App.js
@@ -1,39 +1,24 @@
-import './App.css';
-import Heading from './Heading';
-import {Subject} from './Subject';
-import {Card} from './Subject';
+import React from "react";
+import Fruits from "./Fruits";
+import FruitsCounter from "./FruitsCounter";
 
 function App() {
+  // Підняли стан до компонента App
+  const [fruits] = React.useState([
+    { fruitName: "apple", id: 1 },
+    { fruitName: "apple", id: 2 },
+    { fruitName: "plum", id: 3 },
+  ]);
+
   return (
-    <div className="subject">
-      <Heading firstName="Ярослав" />
-      <h1>Список ваших дисциплін у семестрі:</h1>
-      <Subject subjectName="React Native" description="A framework for web-development"></Subject>
-      <Card description="Вибіркова дисципліна"></Card>
-      <Subject subjectName="Java Script" description="A program language for web-development"></Subject>
-      <Card description="Вибіркова дисципліна"></Card>
-      <Subject subjectName="Web-design" description="subject about creating web-sites"></Subject>
-      <Card description="Обов'язкова дисципліна"></Card>
-      <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Вкажіть будь-яке число від 1 до 5</h1>
-      <button onClick={handleClick} style={{ padding: "10px 20px", fontSize: "16px" }}>
-        Почати
-      </button>
+    <div className="App">
+      <h1>Куди має йти стан?</h1>
+      {/* Передаємо стан до обох компонентів через пропси */}
+      <Fruits fruits={fruits} />
+      <FruitsCounter fruits={fruits} />
     </div>
-    </div>
-    
   );
-  function handleClick() {
-    // Зчитуємо число від користувача через prompt
-    const userNumber = prompt("Введіть число від 1 до 5");
-
-    // Генеруємо випадкове число від 1 до 5
-    const computerNumber = Math.floor(Math.random() * 5) + 1;
-
-    // Відображаємо результат через alert
-    alert(`Ви вказали число: ${userNumber}, число комп’ютера: ${computerNumber}`);
-  }
-
 }
+
 
 export default App;
