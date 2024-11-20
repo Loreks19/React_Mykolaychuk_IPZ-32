fc224fa4232a3d9ac3c488fb6f6e08369777529e
Loreks19 <brawl.boy444@gmail.com>
2024-11-20 18:40:26 +0200

    lab4 task1

--- lab3/task1/src/App.js
+++ lab3/task1/src/App.js
@@ -14,8 +14,26 @@ function App() {
       <Card description="Вибіркова дисципліна"></Card>
       <Subject subjectName="Web-design" description="subject about creating web-sites"></Subject>
       <Card description="Обов'язкова дисципліна"></Card>
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Вкажіть будь-яке число від 1 до 5</h1>
+      <button onClick={handleClick} style={{ padding: "10px 20px", fontSize: "16px" }}>
+        Почати
+      </button>
     </div>
+    </div>
+    
   );
+  function handleClick() {
+    // Зчитуємо число від користувача через prompt
+    const userNumber = prompt("Введіть число від 1 до 5");
+
+    // Генеруємо випадкове число від 1 до 5
+    const computerNumber = Math.floor(Math.random() * 5) + 1;
+
+    // Відображаємо результат через alert
+    alert(`Ви вказали число: ${userNumber}, число комп’ютера: ${computerNumber}`);
+  }
+
 }
 
 export default App;
